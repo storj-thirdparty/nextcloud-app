9fb4e612bad560663f6839ee2fcccf40557e5885
Erik van Velzen <erik@evanv.nl>
2022-04-18 04:53:14 +0200

    Add docker container

---
+++ docker/storj.config.php
@@ -0,0 +1,18 @@
+<?php
+
+if (getenv('OBJECTSTORE_STORJ_BUCKET')) {
+	$CONFIG = [
+		'app_install_overwrite' => [
+			'storj',
+		],
+	    'objectstore' => [
+			'class' => \OCA\Storj\StorjObjectStore::class,
+			'arguments' => [
+				// this is the same naming
+				// convention as the other object storage backends.
+				'serialized_access' => getenv('OBJECTSTORE_STORJ_ACCESS_GRANT'),
+				'bucket' => getenv('OBJECTSTORE_STORJ_BUCKET'),
+			]
+		]
+	];
+}
